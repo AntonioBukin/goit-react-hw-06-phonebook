be20059f48f9e903e8d1f55b32feab847e3e163c
AntonioBukin <bav1905@gmail.com>
2023-09-19 12:18:19 +0300

    add code with Redux Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-import contactsReducer from './contacts/contacts-slice';
-import filterReducer from './filter/filter-slice';
+import rootReducer from './root-reducer';
 
-const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+const persistConfig = {
+  key: 'root', //це ключ під якім будемо записувати у localStorage
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+export const persistor = persistStore(store);
+
+// import contactsReducer from './contacts/contacts-slice';
+// import filterReducer from './filter/filter-slice';
 
 // import { createStore } from 'redux';
 
